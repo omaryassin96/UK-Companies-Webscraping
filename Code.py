c681be687a3b0468ef3824a796117ca216fb7cb8
Omar Yassin <68425418+omaryassin96@users.noreply.github.com>
2022-08-17 05:30:13 +0300

    Update Code.py
    Edited 5 lines of code (14,15,16,23,30) only to increase "readability" of my code ^_^

--- Code.py
+++ Code.py
@@ -11,9 +11,10 @@ drive.mount('/content/drive')
 
 # Accessing Desired Website and Finding all Companies' URLS
 arr_links = []
-for i in range(349):
+for i in range(1,350):
   URL = "https://www.great.gov.uk/international/trade/search/?industries=AEROSPACE&industries=ADVANCED_MANUFACTURING&industries=AIRPORTS&industries=AGRICULTURE_HORTICULTURE_AND_FISHERIES&industries=AUTOMOTIVE&industries=BIOTECHNOLOGY_AND_PHARMACEUTICALS&industries=BUSINESS_AND_CONSUMER_SERVICES&industries=CHEMICALS&industries=CLOTHING_FOOTWEAR_AND_FASHION&industries=COMMUNICATIONS&industries=CONSTRUCTION&industries=CREATIVE_AND_MEDIA&industries=EDUCATION_AND_TRAINING&industries=ELECTRONICS_AND_IT_HARDWARE&industries=ENVIRONMENT&industries=FINANCIAL_AND_PROFESSIONAL_SERVICES&industries=FOOD_AND_DRINK&industries=GIFTWARE_JEWELLERY_AND_TABLEWARE&industries=GLOBAL_SPORTS_INFRASTRUCTURE&industries=HEALTHCARE_AND_MEDICAL&industries=HOUSEHOLD_GOODS_FURNITURE_AND_FURNISHINGS&industries=LIFE_SCIENCES&industries=LEISURE_AND_TOURISM&industries=LEGAL_SERVICES&industries=MARINE&industries=MECHANICAL_ELECTRICAL_AND_PROCESS_ENGINEERING&industries=METALLURGICAL_PROCESS_PLANT&industries=METALS_MINERALS_AND_MATERIALS&industries=MINING&industries=OIL_AND_GAS&industries=PORTS_AND_LOGISTICS&industries=POWER&industries=RAILWAYS&industries=RENEWABLE_ENERGY&industries=RETAIL_AND_LUXURY&industries=SECURITY&industries=SOFTWARE_AND_COMPUTER_SERVICES&industries=TEXTILES_INTERIOR_TEXTILES_AND_CARPETS&industries=WATER&page="
-  website = URL + str(i)  try:
+  website = URL + str(i)
+  try:
     # Creating a Soup out of the Desired Website:
     req = url.Request(website, headers={'User-Agent' : 'Magic Browser'})
     con = url.urlopen(req)
