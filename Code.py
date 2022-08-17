464b04a8e49e7ae809d9ac311a808c6fed8e5ff1
Omar Yassin <68425418+omaryassin96@users.noreply.github.com>
2022-08-17 05:28:07 +0300

    Update Code.py
    Edited 4 code lines (15,16,22,29) only to increase the readability of the code ^_^

--- Code.py
+++ Code.py
@@ -12,21 +12,21 @@ drive.mount('/content/drive')
 # Accessing Desired Website and Finding all Companies' URLS
 arr_links = []
 for i in range(349):
-  website = "https://www.great.gov.uk/international/trade/search/?industries=AEROSPACE&industries=ADVANCED_MANUFACTURING&industries=AIRPORTS&industries=AGRICULTURE_HORTICULTURE_AND_FISHERIES&industries=AUTOMOTIVE&industries=BIOTECHNOLOGY_AND_PHARMACEUTICALS&industries=BUSINESS_AND_CONSUMER_SERVICES&industries=CHEMICALS&industries=CLOTHING_FOOTWEAR_AND_FASHION&industries=COMMUNICATIONS&industries=CONSTRUCTION&industries=CREATIVE_AND_MEDIA&industries=EDUCATION_AND_TRAINING&industries=ELECTRONICS_AND_IT_HARDWARE&industries=ENVIRONMENT&industries=FINANCIAL_AND_PROFESSIONAL_SERVICES&industries=FOOD_AND_DRINK&industries=GIFTWARE_JEWELLERY_AND_TABLEWARE&industries=GLOBAL_SPORTS_INFRASTRUCTURE&industries=HEALTHCARE_AND_MEDICAL&industries=HOUSEHOLD_GOODS_FURNITURE_AND_FURNISHINGS&industries=LIFE_SCIENCES&industries=LEISURE_AND_TOURISM&industries=LEGAL_SERVICES&industries=MARINE&industries=MECHANICAL_ELECTRICAL_AND_PROCESS_ENGINEERING&industries=METALLURGICAL_PROCESS_PLANT&industries=METALS_MINERALS_AND_MATERIALS&industries=MINING&industries=OIL_AND_GAS&industries=PORTS_AND_LOGISTICS&industries=POWER&industries=RAILWAYS&industries=RENEWABLE_ENERGY&industries=RETAIL_AND_LUXURY&industries=SECURITY&industries=SOFTWARE_AND_COMPUTER_SERVICES&industries=TEXTILES_INTERIOR_TEXTILES_AND_CARPETS&industries=WATER&page="+str(i+1)
-  try:
+  URL = "https://www.great.gov.uk/international/trade/search/?industries=AEROSPACE&industries=ADVANCED_MANUFACTURING&industries=AIRPORTS&industries=AGRICULTURE_HORTICULTURE_AND_FISHERIES&industries=AUTOMOTIVE&industries=BIOTECHNOLOGY_AND_PHARMACEUTICALS&industries=BUSINESS_AND_CONSUMER_SERVICES&industries=CHEMICALS&industries=CLOTHING_FOOTWEAR_AND_FASHION&industries=COMMUNICATIONS&industries=CONSTRUCTION&industries=CREATIVE_AND_MEDIA&industries=EDUCATION_AND_TRAINING&industries=ELECTRONICS_AND_IT_HARDWARE&industries=ENVIRONMENT&industries=FINANCIAL_AND_PROFESSIONAL_SERVICES&industries=FOOD_AND_DRINK&industries=GIFTWARE_JEWELLERY_AND_TABLEWARE&industries=GLOBAL_SPORTS_INFRASTRUCTURE&industries=HEALTHCARE_AND_MEDICAL&industries=HOUSEHOLD_GOODS_FURNITURE_AND_FURNISHINGS&industries=LIFE_SCIENCES&industries=LEISURE_AND_TOURISM&industries=LEGAL_SERVICES&industries=MARINE&industries=MECHANICAL_ELECTRICAL_AND_PROCESS_ENGINEERING&industries=METALLURGICAL_PROCESS_PLANT&industries=METALS_MINERALS_AND_MATERIALS&industries=MINING&industries=OIL_AND_GAS&industries=PORTS_AND_LOGISTICS&industries=POWER&industries=RAILWAYS&industries=RENEWABLE_ENERGY&industries=RETAIL_AND_LUXURY&industries=SECURITY&industries=SOFTWARE_AND_COMPUTER_SERVICES&industries=TEXTILES_INTERIOR_TEXTILES_AND_CARPETS&industries=WATER&page="
+  website = URL + str(i)  try:
     # Creating a Soup out of the Desired Website:
     req = url.Request(website, headers={'User-Agent' : 'Magic Browser'})
     con = url.urlopen(req)
     soup = bs(con.read())
     # Printing Page Number to Check Live Progress:
-    print(i+1)
+    print(i)
     # Checking for URLs inside Companies' Cards:
     companies_urls = soup.find('div',{'id':'companies-column'}).find('ul').findAll('li')
     for company_url in companies_urls:
       arr_links.append("https://www.great.gov.uk"+company_url.a['href'])
   except:
     # Printing Page Loading Errors (if any):
-    print(str(i+1) + " ERROR")
+    print(str(i) + " ERROR")
     pass
     
 # Extracting Names, Number of Employees, and Descriptions
